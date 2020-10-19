a674c73e1c8ab63d058bc2feb866d25c89b9b73a
Jordan Fritzen <s0419087@student.rockvalleycollege.edu>
2020-10-19 08:09:50 -0700

    Initial Commit

---
+++ MyContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyContacts
+//
+//  Created by user179545 on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
